a4a62a5cc669f1f7d10e9d668d3978718b978a5f
shalithacode <shalithalakruwan1998@gmail.com>
2022-04-14 07:31:18 +0530

    Added ModalBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,77 @@
 import 'package:flutter/material.dart';
-import './widget/user_transaction.dart';
+import './widget/new_transaction.dart';
+import './widget/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> _transaction = [
+    Transaction(
+      id: '1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+  void _addNewTransaction(String title, double amount) {
+    final newTX = Transaction(
+        id: _transaction.length.toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    setState(() {
+      _transaction.add(newTX);
+    });
+  }
+
   // String? titleInput;
-  // String? amountInput;
+  void startNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter App"),
+          actions: [
+            Builder(builder: (context) {
+              return IconButton(
+                onPressed: () {
+                  startNewTransaction(context);
+                },
+                icon: Icon(Icons.add),
+              );
+            }),
+          ],
         ),
         body: SingleChildScrollView(
           child: Column(
@@ -32,9 +86,18 @@ class MyApp extends StatelessWidget {
                     elevation: 5,
                   ),
                 ),
-                UserTransaction(),
+                TransactionList(_transaction),
               ]),
         ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Builder(builder: (context) {
+          return FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              startNewTransaction(context);
+            },
+          );
+        }),
       ),
     );
   }
